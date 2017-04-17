84316caec8d4c82595cf5e7e31da1699426618f6
hmj <445933532@qq.com>
2017-04-17 22:56:19 +0800

    avoid using "==" operator for mixed types

---
+++ src/page4/equal_operator.js
@@ -0,0 +1,16 @@
+//primitive type is better than wrapper object
+function log(str) {
+    window.console.log(str);
+}
+
+log('"1.0e0" == {valueOf: function () { return true;}} //' + ("1.0e0" == {valueOf: function () { return true;}}));
+log('null == undefined // '+ (null == undefined));
+log('null == "sss" // '+ (null == "sss"));
+log('undefined == "sss" // '+ (undefined == "sss"));
+log('undefined == NaN // '+ (undefined == NaN));
+log('undefined == true // '+ (undefined == true));
+log('undefined == 1 // '+ (undefined == 1));
+
+var date = new Date("2017/4/15");
+log('date == "2017/4/15" //' + (date == "2017/4/15"));
+log('date.toDateString() = ' + date.toDateString());
